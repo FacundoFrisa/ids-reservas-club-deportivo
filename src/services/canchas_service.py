930f01480dd67bea698386f2ce96def6b24d04ec
Matías Ozores <mozores@fi.uba.ar>
2026-09-26 00:50:39 -0300

    refactor: mover conversiones de tipos al repositorio

--- src/services/canchas_service.py
+++ src/services/canchas_service.py
@@ -14,11 +14,6 @@ def obtener_canchas(filtros, limit, offset):
 
 def obtener_canchas(filtros, limit, offset):
     canchas, total_records = CanchasRepository.buscar_canchas(filtros, limit, offset)
-
-    for cancha in canchas:
-        cancha["techada"] = bool(cancha["techada"])
-        cancha["activa"] = bool(cancha["activa"])
-
     return canchas, total_records
 
 def crear_cancha(data):
